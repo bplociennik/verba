875da20b719b353b169156afd04ecb89cd851364
Bartosz Płóciennik <vThaian@users.noreply.github.com>
2020-06-02 19:07:52 +0200

    Add Github CI support (#2)

--- src/config/test_settings.py
+++
@@ -1,9 +0,0 @@
-from .settings import *  # noqa
-
-
-DEBUG = False
-SECRET_KEY = "zvh+vsa)c7$)(z@z7+c!c%ex1k8lqm)*6eh4l#a(t-_m@-i_9&"
-SITE_ID = 1
-
-# --- DATABASE ---
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite3"}}
